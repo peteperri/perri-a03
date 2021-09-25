cace93eddb68241a3366d3e95026ab4b36156316
peteperri <peteperri4@gmail.com>
2021-09-25 19:14:21 -0400

    did ex24 fully

--- exercise24/src/main/java/baseline/Solution24.java
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -10,6 +10,7 @@ Implement the program using a function called isAnagram, which takes in two word
  */
 
 package baseline;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution24 {
@@ -20,44 +21,64 @@ public class Solution24 {
         Solution24 app = new Solution24();
 
         System.out.println("Enter two strings and I'll tell you if they are anagrams or not.");
+
         //get two strings from user, assign them to string variables
-        String a = app.getStringFromUser();
-        String b = app.getStringFromUser();
+        String a = app.getStringFromUser(1);
+        String b = app.getStringFromUser(2);
 
         //pass the two strings to the anagram checker function
         boolean anagram = app.isAnagram(a, b);
 
         //pass the anagram boolean to a function that will determine output
         app.makeOutput(anagram, a, b);
-
     }
 
-    private String getStringFromUser() {
-        String str = null;
-        //if this is the first string:
-        System.out.println("Enter a string: ");
-        //if this is the second string:
-        System.out.println("Enter another string: ");
+    private String getStringFromUser(int num) {
+        String str;
+        if(num == 2) {
+            //if this is the second string:
+            System.out.println("Enter another string: ");
+        }
+        else {
+            //if this is the first (or any other) string:
+            System.out.println("Enter a string: ");
+        }
+        str = input.nextLine();
         return str;
     }
 
-    private boolean isAnagram(String a, String b) {
-        /*
-        check the length of the strings
-        if the strings aren't the same length, return false
-        else:
-        sort both strings alphabetically
-        compare both strings
-        if the strings are equal return true
-        otherwise return false
-        */
-        return false;
+    public boolean isAnagram(String a, String b) {
+        char[] str1 = prepString(a);
+        char[] str2 = prepString(b);
+
+        //compare both strings
+        for (int i = 0; i < str1.length; i++) {
+            //if any characters are not equal, break from loop and return false
+            if (str1[i] != str2[i]) {
+                return false;
+            }
+        }
+        //otherwise, return true
+        return true;
+    }
+
+    private char[] prepString(String str) {
+        //remove spaces from the string, make the string fully lowercase, and convert to char array
+        char[] charArray = str.replaceAll("\\s","").toLowerCase().toCharArray();
+
+        //sort char array alphabetically
+        Arrays.sort(charArray);
+
+        return charArray;
     }
 
     private void makeOutput(boolean anagram, String a, String b){
-        //if the strings are anagrams
-        System.out.printf("%s and %s are anagrams.", a, b);
-        //if the strings aren't anagrams
-        System.out.printf("%s and %s aren't anagrams.", a, b);
+        if(anagram){
+            System.out.printf("%s and %s are anagrams.", a, b);
+        }
+        else{
+            //if the strings aren't anagrams
+            System.out.printf("%s and %s aren't anagrams.", a, b);
+        }
     }
 }
---
+++ exercise24/src/test/java/baseline/Solution24Test.java
@@ -0,0 +1,35 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution24Test {
+
+    Solution24 tester = new Solution24();
+
+    @Test
+    void isAnagram(){
+        //basic anagram test
+        String str1 = "i am lord voldemort";
+        String str2 = "tom marvolo riddle";
+
+        //anagram test with capitalization
+        String str3 = "I am Lord Voldemort";
+        String str4 = "Tom Marvolo Riddle";
+
+        //anagram test that should return false
+        String str5 = "I am not an anagram";
+        String str6 = "I so am an anagram!";
+
+        //anagram test that should return false due to different lengths
+        String str7 = "We are not the same length.";
+        String str8 = "We are not.";
+
+        assertAll(
+                () -> assertTrue(tester.isAnagram(str1, str2)),
+                () -> assertTrue(tester.isAnagram(str3, str4)),
+                () -> assertFalse(tester.isAnagram(str5, str6)),
+                () -> assertFalse(tester.isAnagram(str7, str8)));
+    }
+}
