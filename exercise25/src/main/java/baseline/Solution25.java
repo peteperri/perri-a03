77aa883a99564a230bd058dd0da86be37b6ffd08
peteperri <peteperri4@gmail.com>
2021-09-25 22:13:15 -0400

    ex25 completed

--- exercise25/src/main/java/baseline/Solution25.java
+++ exercise25/src/main/java/baseline/Solution25.java
@@ -23,35 +23,86 @@ import java.util.Scanner;
 
 public class Solution25 {
 
-    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args){
+        Solution25 app = new Solution25();
         //call getPasswordFromUser to get the password
+        String pw = app.getPasswordFromUser();
         //pass password to passwordValidator, get strengthLevel
+        int strengthLevel = app.passwordValidator(pw);
         //call outputCreator, which creates output based on strengthLevel
+        app.outputCreator(strengthLevel);
     }
 
     private String getPasswordFromUser(){
         //prompt the user for a password
+        System.out.println("Enter the password: ");
         //assign the password to a variable and return it
-        String password = null;
-        return password;
+        return input.nextLine();
     }
 
     public int passwordValidator(String pw){
+        int strengthLevel;
+        boolean containsNumber = false;
+        boolean containsLetter = false;
+        boolean containsSpecial = false;
+        char[] charArray = pw.toCharArray();
+        for(char c : charArray) {
+            if (Character.isDigit(c)) {
+                containsNumber = true;
+            }
+            else if (Character.isAlphabetic(c)) {
+                containsLetter = true;
+            }
+            else if((!Character.isAlphabetic(c)) && !(Character.isDigit(c))){
+                containsSpecial = true;
+            }
+        }
+
         //if the password only has numbers and is less than 8 characters, strengthLevel = 1
+        if (containsNumber && !containsLetter && charArray.length < 8){
+            strengthLevel = 1;
+        }
         //else if the password only has letters and is less than 8 characters, strengthLevel = 2
+        else if (containsLetter && !containsNumber && charArray.length < 8){
+            strengthLevel = 2;
+        }
         //else if the password has letters, numbers, and is at least 8 characters, strengthLevel = 3
+        else if(containsLetter && containsNumber && !containsSpecial && charArray.length >= 8){
+            strengthLevel = 3;
+        }
         //else if the password has letters, numbers, and special characters and is at least 8 characters, strengthLevel = 4
+        else if (containsLetter && containsNumber && containsSpecial && charArray.length >= 8){
+            strengthLevel = 4;
+        }
         //else strengthLevel = 0
-        int strengthLevel = 0;
+        else {
+            strengthLevel = 0;
+        }
         return strengthLevel;
     }
 
     private void outputCreator(int strength){
         //if strengthLevel is 1 print that the password is very weak
+        if(strength == 1){
+            System.out.println("Password is very weak.");
+        }
         //else if 2 print weak
+        else if(strength == 2){
+            System.out.println("Password is weak.");
+        }
         //else if 3 print strong
+        else if(strength == 3){
+            System.out.println("Password is strong.");
+        }
         //else if 4 print very strong
+        else if(strength == 4){
+            System.out.println("Password is very strong.");
+        }
         //else print unknown strength
+        else{
+            System.out.println("Password is of unknown strength.");
+        }
     }
-
 }
---
+++ exercise25/src/test/java/baseline/Solution25Test.java
@@ -0,0 +1,18 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution25Test {
+
+    @Test
+    void passwordValidator() {
+        Solution25 tester = new Solution25();
+        assertEquals(1, tester.passwordValidator("1234"));
+        assertEquals(2, tester.passwordValidator("abcde"));
+        assertEquals(3, tester.passwordValidator("abcde12345"));
+        assertEquals(4, tester.passwordValidator("abcde452!@%*&$(#"));
+        assertEquals(0, tester.passwordValidator("12ab"));
+    }
+}
