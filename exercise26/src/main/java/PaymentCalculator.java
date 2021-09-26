578b94f532741e429c9a38ec7028044472f01d48
peteperri <peteperri4@gmail.com>
2021-09-26 15:11:45 -0400

    ex26 pseudo

---
+++ exercise26/src/main/java/PaymentCalculator.java
@@ -0,0 +1,17 @@
+public class PaymentCalculator {
+
+    double balance;
+    int aprPercent;
+    double monthlyPayment;
+
+    public PaymentCalculator(double balance, int aprPercent, double monthlyPayment){
+        //constructor
+        balance = this.balance;
+        aprPercent = this.aprPercent;
+        monthlyPayment = this.monthlyPayment;
+    }
+    public int calculateMonthsUntilPaidOff(){
+        //use balance, aprPercent, and monthlyPayment to calculate the amount of months left, and return it
+        return 0;
+    }
+}
