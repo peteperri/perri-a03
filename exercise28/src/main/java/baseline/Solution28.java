2ff2101eab9c82fa399622d4d4d9a01605abcb8a
peteperri <peteperri4@gmail.com>
2021-09-27 15:11:28 -0400

    ex 28 redux

--- exercise28/src/main/java/baseline/Solution28.java
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -1,24 +1,44 @@
-import java.util.Scanner;
-
 /*
  *  UCF COP3330 Fall 2021 Assignment 3 Solutions
  *  Copyright 2021 first_name last_name
  */
+package baseline;
+import java.util.Scanner;
+
 public class Solution28 {
     Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
         Solution28 app = new Solution28();
-        int num = 0;
-        app.addFiveNumbers(num);
+
+        //declare int array num with a size of 5
+        int[] num;
+        num = new int[5];
+
+        //set sum equal to a call to addFiveNumbers(num)
+        int sum = app.getFiveNumbers(num);
+
+        //send sum to the makeOutput function
+        app.makeOutput(sum);
     }
-    public int addFiveNumbers(int num){
-       for(int i = 0; i < 5; i++){
-           System.out.println("Enter a number: ");
-           num = num + input.nextInt();
+
+    public int getFiveNumbers(int[] num){
+        //for i = 0, i < 5, i++, ask the user for a number, then add that number to num array
+        for(int i = 0; i < 5; i++){
+            System.out.println("Enter an integer (whole number): ");
+            num[i] = input.nextInt();
         }
-       return num;
+       return addFiveNumbers(num);
     }
-    public void makeOutput(int num){
-        System.out.println("The total is " + num);
+    public int addFiveNumbers(int[] num){
+        //add all numbers in num array
+        int sum = 0;
+        for(int i = 0; i < 5; i++){
+            sum = sum + num[i];
+        }
+        return sum;
+    }
+
+    private void makeOutput(int sum){
+        System.out.println("The total is " + sum);
     }
 }
--- exercise28/src/test/java/baseline/Solution28Test.java
+++ exercise28/src/test/java/baseline/Solution28Test.java
@@ -1,4 +1,17 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class Solution28Test {
-  
+
+    @Test
+    void addFiveNumbers() {
+        Solution28 tester = new Solution28();
+        int[] num = {1, 1, 1, 1, 1};
+        assertEquals(5, tester.addFiveNumbers(num));
+        int[] num2 = {5, 10, 15, 20, 25};
+        assertEquals(75, tester.addFiveNumbers(num2));
+    }
 }
