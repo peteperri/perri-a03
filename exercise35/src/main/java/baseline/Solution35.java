9f42ae6fde5eb659621a9a65d74e765803c18c31
peteperri <peteperri4@gmail.com>
2021-09-26 23:35:29 -0400

    ex35 pseudo

---
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -0,0 +1,24 @@
+package baseline;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Solution35 {
+    public static void main(String[] args) {
+        //call getUserInput, assign value to new ArrayList
+        //call pickWinner, assign value to String winner
+        //pass winner to createOutput function
+    }
+    private ArrayList<String> getUserInput(){
+        //asks user for a name until they leave an entry blank
+        //adds each name to the arrayList
+        //return the arrayList
+    }
+    public String pickWinner(ArrayList<String> contestants){
+        //generate random integer between 1 and ArrayList size
+        //return the ArrayList entry at the randomly generated index
+    }
+    private void createOutput(String winner){
+        //print the winner
+    }
+}
