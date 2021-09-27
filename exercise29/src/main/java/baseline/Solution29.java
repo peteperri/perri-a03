75fca8aa06c11cea17cdf382838a274583d657d9
peteperri <peteperri4@gmail.com>
2021-09-26 20:59:32 -0400

    29 pseudocode retry

--- exercise29/src/main/java/baseline/Solution29.java
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -1,2 +1,21 @@
-package baseline;public class Solution29 {
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution29 {
+
+    Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        //call promptUser to validate input, then promptUser will call calcYears once input is validated
+    }
+
+    private void promptUser(){
+        //while boolean invalid is true, continue to prompt the user for new data.
+        //boolean invalid is true if the data the user enters is not a number, or 0.
+        //when invalid is false, call calcYears
+    }
+    public int calcYears(int r){
+        //calculates the number of years to double investment
+    }
 }
