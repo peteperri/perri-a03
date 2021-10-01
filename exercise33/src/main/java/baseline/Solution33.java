6057f2e6e3d9cd88c0fb6b7be2adb039bcecfee1
peteperri <peteperri4@gmail.com>
2021-10-01 19:33:28 -0400

    ex33 finished

--- exercise33/src/main/java/baseline/Solution33.java
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -1,19 +1,50 @@
 package baseline;
 
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Solution33 {
+    Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
+        Solution33 app = new Solution33();
+        boolean gaming = true;
         //while boolean gaming is true, prompt the user to enter questions
-        //get random answer from getAnswer(), print it
-        //ask the user if they want to ask another question
-        //if yes repeat, else end the program
+        while(gaming){
+           app.askQuestion();
+            //get random answer from getAnswer(), print it
+           System.out.println(app.getAnswer());
+            //ask the user if they want to ask another question
+           gaming = app.askAgain();
+        }
     }
 
     public String getAnswer(){
+        String[] answers = {"Yes", "No", "Maybe", "Ask again later"};
         //use ThreadLocalRandom.current().nextInt(1, 4 + 1); to generate a random number between 1 and 4
+        int answerInt = ThreadLocalRandom.current().nextInt(1, 4 + 1);
         //series of if statements; each number 1 through 4 refers to an answer option
-        return "";
+        if(answerInt == 1){
+            return answers[0];
+        }
+        else if(answerInt == 2){
+            return answers[1];
+        }
+        else if(answerInt == 3){
+            return answers[2];
+        }
+        return answers[3];
     }
     private void askQuestion(){
-        //prompt the user to ask a question, then do absolutely nothing with it and call getAnswer()
+        System.out.println("What's your question?");
+        input.nextLine();
+    }
+
+    private boolean askAgain(){
+        //if yes repeat, else end the program
+        System.out.println("Ask another question?  y/n");
+        boolean playAgain = input.hasNext("y") || input.hasNext("Y") || input.hasNext("yes" )|| input.hasNext("Yes" );
+        input.nextLine();
+        return playAgain;
     }
 }
