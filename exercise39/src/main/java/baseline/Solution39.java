03e10a0b52f1df638d741f9ebf0ec4a5e591fe5b
peteperri <peteperri4@gmail.com>
2021-10-03 01:36:49 -0400

    ex39 pseudo

---
+++ exercise39/src/main/java/baseline/Solution39.java
@@ -0,0 +1,39 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Peter Perri
+ */
+
+package baseline;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class Solution39 {
+    public static void main(String[] args) {
+        //create an array of Strings to hold the last names, aka the keys
+        //initialize an ArrayList of HashMaps called "data", and add each HashMap to it
+        //each HashMap is filled by a respective function that receives the list of keys, since all of them use the same keys
+        //call Arrays.sort to sort the list of keys alphabetically
+        //call the printTable function to print the output;
+    }
+    public HashMap makeFirstNameMap(String[] keys){
+        //fill the hashmap with firstNames based on the keys
+        //return the hashmap
+    }
+
+    public HashMap makePositionMap(String[] keys){
+
+    }
+
+    public HashMap makeSeparationDateMap(String[] keys){
+
+    }
+
+    public void printTable(List<HashMap<String, String>> employees, String[] keys){
+        //assign each element of the array to its respective hashmap from the list
+        //for loop that iterates through every element of the keys array and prints each hashmap for that respective key
+        }
+    }
+}
