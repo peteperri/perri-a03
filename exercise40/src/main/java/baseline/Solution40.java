96b991b4f8962f1de3acd52b631603731e07c2b4
peteperri <peteperri4@gmail.com>
2021-10-03 16:00:03 -0400

    ex40 pseudo

---
+++ exercise40/src/main/java/baseline/Solution40.java
@@ -0,0 +1,85 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Peter Perri
+ */
+
+package baseline;
+
+import java.util.*;
+
+public class Solution40 {
+    public static void main(String[] args) {
+        Solution40 app = new Solution40();
+        //create an array of Strings to hold the last names, aka the keys
+
+        //initialize an ArrayList of HashMaps called "data", and add each HashMap to it
+        //each HashMap is filled by a respective function that receives the list of keys, since all of them use the same keys
+
+        //call the getSearchString function to prompt the user for a searchString
+
+        //call the searchTable function to search the table. this function will return an arraylist of searched strings
+
+        //call the printTable function to print the output
+
+    }
+    public HashMap makeFirstNameMap(String[] keys){
+        //fill the hashmap with firstNames based on the keys
+        HashMap<String, String> firstNames = new HashMap<>();
+        firstNames.put(keys[0],"John");
+        firstNames.put(keys[1],"Tou");
+        firstNames.put(keys[2],"Michaela");
+        firstNames.put(keys[3], "Jake");
+        firstNames.put(keys[4], "Jacquelyn");
+        firstNames.put(keys[5], "Sally");
+        //return the hashmap
+        return firstNames;
+    }
+
+    public HashMap makePositionMap(String[] keys){
+        HashMap<String, String> positions = new HashMap<>();
+        positions.put(keys[0], "Manager");
+        positions.put(keys[1], "Software Engineer");
+        positions.put(keys[2], "District Manager");
+        positions.put(keys[3], "Programmer");
+        positions.put(keys[4], "DBA");
+        positions.put(keys[5], "Web Developer");
+        return positions;
+    }
+
+    public HashMap makeSeparationDateMap(String[] keys){
+        HashMap<String, String> separationDate = new HashMap<>();
+        separationDate.put(keys[0], "2016-12-31");
+        separationDate.put(keys[1], "2016-10-05");
+        separationDate.put(keys[2], "2015-12-19");
+        separationDate.put(keys[3], " ");
+        separationDate.put(keys[4], " ");
+        separationDate.put(keys[5], "2015-12-18");
+        return separationDate;
+    }
+
+    private String getSearchString(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the search string: ");
+        return input.nextLine();
+    }
+
+    public ArrayList<String> searchTable(List<HashMap<String, String>> employees, String[] keys, String searchString){
+
+        //for loop for each key in keys
+        //compare each firstName and each key(lastNames) to the search string
+        //if either contains the searchString, then add it to the list of searched keys and return the searchedKeys
+    }
+
+    private void printTable(List<HashMap<String, String>> employees, String[] keys, ArrayList<String> searchedKeys){
+        //assign each element of the array to its respective hashmap from the list
+
+        //for loop to print out the searchedKeys and their matching data
+
+    }
+
+
+
+
+
+
+}
